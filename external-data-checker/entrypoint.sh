6b0e08dfe4694d20059473711e67a28bb4383e26
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2019-10-25 10:18:24 +0200

    external-data-checker: Initial commit

---
+++ external-data-checker/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ -z "$GITHUB_WORKSPACE" || -z "$GITHUB_REPOSITORY" ]]; then
+    echo "Script is not running in GitHub Actions CI"
+    exit 1
+fi
+
+cd $GITHUB_WORKSPACE
+
+APP_ID=${GITHUB_REPOSITORY#flathub/}
+if [[ -f ${APP_ID}.yml ]]; then
+    manifest=${APP_ID}.yml
+elif [[ -f ${APP_ID}.yaml ]]; then
+    manifest=${APP_ID}.yml
+elif [[ -f ${APP_ID}.json ]]; then
+    manifest=${APP_ID}.json
+else
+    echo "Couldn't find manifest file"
+    exit 1
+fi
+
+/opt/flatpak-external-data-checker/flatpak-external-data-checker $manifest
