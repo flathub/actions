f54d26ac3407ab5199be157a6cd466a410db56d1
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2019-11-21 11:42:54 +0100

    flathub-data-checker: Initial commit
    Add global variant of external-data-checker which clones entire Flathub
    organization and runs the tool on all manifests. This is workaround
    for GitHub missing API for defining secrets for repositories and
    a way to define an action for all repositories in organization.
    
    At the moment it performs a dry run printing status on stdout, after
    some testing it'll be changed to use --update switch.

---
+++ flathub-data-checker/entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ -z "$GITHUB_WORKSPACE" || -z "$GITHUB_REPOSITORY" ]]; then
+    echo "Script is not running in GitHub Actions CI"
+    exit 1
+fi
+
+detect_manifest() {
+    repo=${1}
+
+    if [[ -f $repo/${repo}.yml ]]; then
+        manifest=${repo}.yml
+    elif [[ -f $repo/${repo}.yaml ]]; then
+        manifest=${repo}.yaml
+    elif [[ -f $repo/${repo}.json ]]; then
+        manifest=${repo}.json
+    else
+        return 1
+    fi
+
+    echo $manifest
+}
+
+git config --global user.name "flathubbot" && \
+git config --global user.email "sysadmin@flathub.org"
+
+mkdir flathub
+cd flathub
+
+gh-ls-org flathub | parallel "git clone --depth 1 {}"
+
+for repo in */; do
+    repo=${repo%/}
+    manifest=$(detect_manifest $repo)
+    if [[ -n $manifest ]]; then
+        echo "==> checking ${repo}"
+        /opt/flatpak-external-data-checker/flatpak-external-data-checker $repo/$manifest
+    fi
+done
