6b5ac9393ed9963b74f32d0aaf48705b0fbf08ce
Vincent Chernin <38842733+vchernin@users.noreply.github.com>
2022-05-05 10:34:55 -0700

    f-e-d-c: Check for require-important-update: true in flathub.json We need to pass --require-important-update if an app opts-in via flathub.json See https://github.com/flathub/flatpak-external-data-checker/pull/276

--- flathub-external-data-checker/entrypoint.sh
+++ flathub-external-data-checker/entrypoint.sh
@@ -15,13 +15,20 @@ gh-ls-org flathub | parallel "git clone --depth 1 {}"
 mapfile -t checker_apps < <( grep -rl -E 'extra-data|x-checker-data|\.AppImage' | cut -d/ -f1 | sort -u )
 
 for repo in ${checker_apps[@]}; do
-    # check if repo opted out
+    FEDC_OPTS=()
+
     if [[ -f $repo/flathub.json ]]; then
+        # check if repo opted out
         if ! jq -e '."disable-external-data-checker" | not' < $repo/flathub.json > /dev/null; then
             continue
         fi
+        # check if the app is EOL
         if ! jq -e '."end-of-life" or ."end-of-life-rebase" | not' < $repo/flathub.json > /dev/null; then
             continue
+        fi
+        # add repo-specified f-e-d-c args
+        if jq -e '."require-important-update"' < $repo/flathub.json > /dev/null; then
+            FEDC_OPTS+=("--require-important-update")
         fi
     fi
 
@@ -36,5 +43,5 @@ for repo in ${checker_apps[@]}; do
     fi
 
     echo "==> checking ${repo}"
-    /app/flatpak-external-data-checker --verbose --update "$repo/$manifest"
+    /app/flatpak-external-data-checker --verbose --update "${FEDC_OPTS[@]}" "$repo/$manifest"
 done
