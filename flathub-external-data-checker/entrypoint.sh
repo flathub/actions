dc1c380e291bb8a3c13a0ba0a6a9136b87554823
Vincent Chernin <38842733+vchernin@users.noreply.github.com>
2022-05-05 13:59:45 -0700

    f-e-d-c: Remove detect_manifest() function move detect_manifest() function code directly into loop to allow FEDC_OPTS to be set per loop iteration

--- flathub-external-data-checker/entrypoint.sh
+++ flathub-external-data-checker/entrypoint.sh
@@ -5,32 +5,6 @@ if [[ -z "$GITHUB_WORKSPACE" || -z "$GITHUB_REPOSITORY" ]]; then
     exit 1
 fi
 
-detect_manifest() {
-    repo=${1}
-    
-    # check if repo opted out
-    if [[ -f $repo/flathub.json ]]; then
-        if ! jq -e '."disable-external-data-checker" | not' < $repo/flathub.json > /dev/null; then
-            return 1
-        fi
-        if ! jq -e '."end-of-life" or ."end-of-life-rebase" | not' < $repo/flathub.json > /dev/null; then
-            return 1
-        fi
-    fi
-
-    if [[ -f $repo/${repo}.yml ]]; then
-        manifest=${repo}.yml
-    elif [[ -f $repo/${repo}.yaml ]]; then
-        manifest=${repo}.yaml
-    elif [[ -f $repo/${repo}.json ]]; then
-        manifest=${repo}.json
-    else
-        return 1
-    fi
-
-    echo $manifest
-}
-
 git config --global user.name "flathubbot" && \
 git config --global user.email "sysadmin@flathub.org"
 
@@ -41,9 +15,26 @@ gh-ls-org flathub | parallel "git clone --depth 1 {}"
 mapfile -t checker_apps < <( grep -rl -E 'extra-data|x-checker-data|\.AppImage' | cut -d/ -f1 | sort -u )
 
 for repo in ${checker_apps[@]}; do
-    manifest=$(detect_manifest $repo)
-    if [[ -n $manifest ]]; then
-        echo "==> checking ${repo}"
-        /app/flatpak-external-data-checker --verbose --update $repo/$manifest
+    # check if repo opted out
+    if [[ -f $repo/flathub.json ]]; then
+        if ! jq -e '."disable-external-data-checker" | not' < $repo/flathub.json > /dev/null; then
+            continue
+        fi
+        if ! jq -e '."end-of-life" or ."end-of-life-rebase" | not' < $repo/flathub.json > /dev/null; then
+            continue
+        fi
     fi
+
+    if [[ -f $repo/${repo}.yml ]]; then
+        manifest=${repo}.yml
+    elif [[ -f $repo/${repo}.yaml ]]; then
+        manifest=${repo}.yaml
+    elif [[ -f $repo/${repo}.json ]]; then
+        manifest=${repo}.json
+    else
+        continue
+    fi
+
+    echo "==> checking ${repo}"
+    /app/flatpak-external-data-checker --verbose --update "$repo/$manifest"
 done
